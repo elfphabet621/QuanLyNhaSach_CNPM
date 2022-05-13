3df1984afb6a630a83732cdbabb6587c4b997393
elfphabet621 <elfphabet@gmail.com>
2022-05-13 18:51:07 +0700

    Update basic templates, views, urls - CRUD book - login, register

--- QLNhaSach/book/urls.py
+++ QLNhaSach/book/urls.py
@@ -4,19 +4,19 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     
-    # path('register/', views.registerPage, name='register'),
-    # path('login/', views.loginPage, name='login'),
-    # path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 
     # path('user/', views.userPage, name='user-page'),
 
     # path('account/', views.accountSetting, name='account'),
 
-    # path('products/', views.products, name='products'),
+    # path('book/', views.products, name='products'),
     # path('customer/<str:pk_test>', views.customer, name='customer'),
     
-    # path('create_order/<str:pk>', views.createOrder, name='create_order'),
-    # path('update_order/<str:pk>', views.updateOrder, name='update_order'),
-    # path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
+    path('create_book/<str:pk>', views.createBook, name='create_Book'),
+    path('update_book/<str:pk>', views.updateBook, name='update_Book'),
+    path('delete_book/<str:pk>', views.deleteBook, name='delete_Book'),
 
 ]
--- QLNhaSach/book/views.py
+++ QLNhaSach/book/views.py
@@ -1,6 +1,79 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+# Trang chủ
 def home(request):
+
     # context = {}
     return render(request, 'book/main.html')
+
+# Trang đăng ký tài khoản
+def registerPage(request):
+
+    # if request.method == "POST":
+        
+    #     if form.is_valid():
+    #         messages.success(request, 'Account was created for '+ username)
+    #         return redirect('login')
+
+    context = {}
+    return render(request, 'book/register.html', context)
+
+# Trang đăng nhập
+def loginPage(request):
+    # if request.method == "POST":
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+
+    #     user = authenticate(request, username=username, password=password)
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect('home')
+    #     else:
+    #         messages.info(request, 'Username Or Password is incorrect')
+    context = {}
+    return render(request, 'book/login.html', context)
+
+# Đăng xuất
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+# Tạo một cuốn sách mới
+# @login_required(login_url='login')
+# @allowed_users(allowed_roles=['admin'])
+def createBook(request, ):
+    # if request.method == "POST":
+        # if formset.is_valid():
+        #     formset.save()
+        #     return redirect('/')
+
+    context = {}
+
+    return render(request, 'book/book_form.html', context)
+
+# Cập nhật một cuốn sách
+# @login_required(login_url='login')
+# @allowed_users(allowed_roles=['admin'])
+def updateBook(request):
+    # if request.method == "POST":
+        # if form.is_valid() :
+        #     form.save()
+        #     return redirect('/')
+
+    context = {}
+    return render(request, 'book/book_form.html', context)
+
+# Xóa một cuốn sách
+# @login_required(login_url='login')
+# @allowed_users(allowed_roles=['admin'])
+def deleteBook(request):
+
+    # if request.method == "POST":
+    #     book.delete()
+    #     return redirect('/')
+
+    context = {}
+    return render(request, 'book/delete_book.html', context)
