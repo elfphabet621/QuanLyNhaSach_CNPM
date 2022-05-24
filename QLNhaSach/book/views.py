95bc0f3a09f9862c83c74900e7b10446177d0cd5
luong12320 <nguyenminhluong14092001@gmail.com>
2022-05-24 22:49:01 +0700

    Resolve conflicts

--- QLNhaSach/book/views.py
+++ QLNhaSach/book/views.py
@@ -38,10 +38,7 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-<<<<<<< HEAD
-=======
             
->>>>>>> origin/main
             messages.success(request, 'Account was created for '+ username)
             return redirect('login')
 
