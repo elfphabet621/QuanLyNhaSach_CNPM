ae6cae797fe86b5d66c0dafe84a57503b02538ae
npqd <quangdungreal1712@gmail.com>
2022-06-10 06:13:09 +0700

    inventory report

--- QLNhaSach/book/views.py
+++ QLNhaSach/book/views.py
@@ -12,7 +12,8 @@ from .decorators import unauthenticated_user, allowed_users
 import math, json
 from .filters import BookFilter
 from collections import defaultdict
-import datetime
+from collections import Counter
+from datetime import date
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Trang chủ
@@ -367,31 +368,68 @@ def debt_report(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['thủ kho'])
 def inventory_report(request):
-    # Sách sẽ bắt đầu bán vào tháng 1,2022 => Tồn đầu ở tháng 1 là list tất cả các sách và số lượng
-    # Phát sinh là số sách bán trong tháng, (nhập trong tháng)
-    # Tồn cuối: Tồn đầu + Phát sinh 
-    # Tồn đầu tháng sau bằng tồn cuối tháng trước.
-
-
+    
     book_list = Sach.objects.all()
-    current_year, current_month = 2022, 1
-    hd_month = HoaDon.objects.filter(ngay_lap_HD__year=current_year, 
-                                        ngay_lap_HD__month=current_month)
-
-    #mat_hang, created = ChiTietHoaDon.objects.get_or_create(hoa_don=hd_month, sach=sach)
+    is_empty = False
+    inventory = []
+    select_month, select_year = 6,2022
     if request.method == "POST":
-        current_month = int(request.POST.get('month'))
-        current_year = int(request.POST.get('year'))
-
-        remain_book = defaultdict(int)
-        for i in range(1, current_month):
-            hd_month_i = HoaDon.objects.filter(ngay_lap_HD__year=current_year, ngay_lap_HD__month= i)
+        select_month = int(request.POST.get('month'))
+        select_year = int(request.POST.get('year'))
+        if select_month > date.today().month:
+            is_empty = True
+        sell_book = defaultdict(int)
+        entry_book = defaultdict(int)
+        for i in range(select_month,date.today().month + 1):
+            # Lọc ra số sách bán từ tháng được chọn đến tháng hiện tại. 
+            hd_month_i = HoaDon.objects.filter(ngay_lap_HD__year=select_year, ngay_lap_HD__month= i)
             for hd in hd_month_i:
-                cthd =  hd_month_i.objects.filter(hoadon=hd)
-                for book in book_list():
-                    return 0
+                cthds =  hd_month_i.objects.filter(hoadon=hd)
+                for cthd in cthds:
+                    sell_book[cthd.sach.ten_sach] += cthd.so_luong
 
-    context = {'book_list':book_list}
+            ns_month_i = NhapSach.objects.filter(ngay_nhap__year=select_year, ngay_nhap__month= i)
+            for ns in ns_month_i:
+                entry_book[ns.ten_sach] += ns.so_luong
+        # TỒN ĐẦU: sách hiện tại - phát sinh từ tháng chọn ( với phát sinh = nhập - bán )
+        #TỔNG PHÁT SINH TỪ THÁNG CHỌN ĐẾN HIỆN TẠI:
+
+        phatsinh_n_month_book = {k: entry_book.get(k,0) - sell_book.get(k,0) for k in set(entry_book) | set(sell_book)}
+        for book in book_list:
+            for s, sl in phatsinh_n_month_book.items():
+                if book.ten_sach == s:
+                    book.so_luong -= sl
+
+
+        phatsinh_month = defaultdict(int)
+        for book in book_list:
+            phatsinh_month[book.ten_sach] = 0
+        #PHÁT SINH TRONG THÁNG ĐƯỢC CHỌN:
+        sell_book_month = defaultdict(int)
+        entry_book_month = defaultdict(int)
+        hd_month = HoaDon.objects.filter(ngay_lap_HD__year=select_year, ngay_lap_HD__month= select_month)
+        for hd in hd_month:
+            cthds = hd_month.object.filter(hoadon=hd)
+            for cthd in cthds:
+                sell_book_month[cthd.sach.ten_sach] += cthd.so_luong
+
+        ns_month = NhapSach.objects.filter(ngay_nhap__year=select_year, ngay_nhap__month=select_month)
+        for ns in ns_month:
+            entry_book_month[ns.ten_sach] += ns.so_luong
+
+        phatsinh_month_book = {k: entry_book_month.get(k,0) - sell_book_month.get(k,0) for k in set(entry_book_month) | set(sell_book_month)}
+        phatsinh_1_month = {k: phatsinh_month.get(k,0) + phatsinh_month_book.get(k,0) for k in set(phatsinh_month) | set(phatsinh_month_book)}
+
+        
+        for book in book_list():
+            book_inventory = Inventory(ten_sach= book.ten_sach, ton_dau = book.so_luong, phat_sinh = phatsinh_1_month[book.ten_sach])
+            inventory.append(book_inventory)
+       
+                
+    
+    context = {'inventory':inventory, 'is_empty': is_empty,
+                'months': [i for i in range(1,13)], 'select_month': select_month,
+                'years': [2022], 'select_year': select_year}
     return render(request, 'book/inventory_report.html', context= context)
 
 @allowed_users(allowed_roles=['khách hàng'])
